301092190cd7908fc1e4c0626eb43bdaf9176877
Shaheena <shaheena123@gmail.com>
2026-07-22 19:55:22 +0530

    Improved ShopSphere UI with new features

--- frontend/script.js
+++ frontend/script.js
@@ -141,3 +141,41 @@ products[i].style.display="none";
 
 
 }
+function countdown(){
+
+let endTime = new Date().getTime() + 24*60*60*1000;
+
+
+let timer = setInterval(function(){
+
+let now = new Date().getTime();
+
+let distance = endTime - now;
+
+
+let hours = Math.floor(
+(distance % (1000*60*60*24))/(1000*60*60)
+);
+
+let minutes = Math.floor(
+(distance % (1000*60*60))/(1000*60)
+);
+
+
+let seconds = Math.floor(
+(distance % (1000*60))/1000
+);
+
+
+document.getElementById("timer").innerHTML =
+hours + "h : " +
+minutes + "m : " +
+seconds + "s";
+
+
+},1000);
+
+}
+
+
+countdown();
